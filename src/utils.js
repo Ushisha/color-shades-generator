6e5a5e20b6ee2b2c33a1399ea2f6c16c2fa28a1e
ushisha <usisha.s@gmail.com>
2021-07-12 07:28:29 +0100

    clean up & add gitignore file

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-function componentToHex(c){
-    var hex = c.toString(16);
-    return hex.length === 1? '0' + hex: hex;
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length === 1 ? "0" + hex : hex;
 }
 
-function rgbToHex(r,g,b) {
-    return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b)
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 export default rgbToHex;
